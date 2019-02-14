a49d99bd62d261116abadedfb87d60e4f9fa70b2
lizhi <lizhi1026@126.com>
2019-02-14 18:42:04 +0800

    init left view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,22 @@ import './style.css';
 
 class Header extends Component {
     render(){
-        return (<header>
-            <div></div>
-        </header>);
+        return (<div className="controller">
+            <div className="method"><span>POST</span></div>
+            <div className="host">
+                <input 
+                placeholder='请输入请求的host'
+                className="input">
+                </input>
+            </div>
+            <div className="url">
+                <input
+                placeholder='请输入请求的url'
+                className="input">
+                </input>
+                </div>
+            <div className="send"><span>发送</span></div>
+        </div>);
     }
 }
 
--- src/components/Left/index.js
+++ src/components/Left/index.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
 import './styls.css';
 
+const list = [1,2,3,4,5,6,7,7,7,7,7,7,7,7,7,77,7,7,7,7,7,7,7,7,7];
+
 class Left extends Component {
     render(){
-        return (<header>
-            <div></div>
-        </header>);
+        return (
+        <div>
+            <div className="title">
+                <span>文档列表</span>
+            </div>
+            <div className="content">
+                {this.renderRepoList()}
+            </div>
+        </div>
+        );
     }
+
+    renderRepoList= () => {
+        const repos = list;
+        if (!repos) {
+            return null;
+        }
+
+        let that = this;
+
+        return repos.map((item, index) => {
+            return (
+                <div>
+                    <div><span>title</span></div>
+                    {that.renderDocList(repos)}
+                </div>
+            );
+        })
+    }
+
+    renderDocList= (list) => {
+        const docs = list;
+        if (!docs) {
+            return null;
+        }
+
+        return docs.map((item, index) => {
+            return (
+                <li>
+                    <span>content</span>
+                </li>
+            );
+        })
+    }
+
 }
 
 
