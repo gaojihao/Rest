6745e1c846cdb57d3527f58cab21e3f0e2fa7ca6
lizhi <lizhi1026@126.com>
2019-02-14 23:22:44 +0800

    add react-json-view

--- src/components/Middle/index.js
+++ src/components/Middle/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './styls.css';
 
+const list = [1,2,3,4,5,6,7,7,7,7,7,7,7,7,7,77,7,7,7,7,7,7,7,7,7];
+
 class Middle extends Component {
     render(){
-        return (<header>
-            <div></div>
-        </header>);
+        return (
+        <div>
+            <div className="title">
+                <span>Request Body</span>
+            </div>
+            <div className="content">
+                {this.renderRequestItem()}
+            </div>
+        </div>
+        );
+    }
+
+    renderRequestItem= () => {
+        const items = list;
+        if (!items) {
+            return null;
+        }
+
+        return items.map((item, index) => {
+            return (
+                <li className="docTitle">
+                    <span>content</span>
+                </li>
+            );
+        })
     }
 }
 
--- src/components/Right/index.js
+++ src/components/Right/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './styls.css';
+import ReactJson from 'react-json-view'
 
 class Right extends Component {
     render(){
-        return (<header>
-            <div></div>
-        </header>);
+        return (
+        <div>
+            <div className="title">
+                <span>Response Header</span>
+            </div>
+            <div>
+                <ReactJson />
+            </div>
+            <div className="title">
+                <span>Response Body</span>
+            </div>
+            <div>
+                <ReactJson />
+            </div>
+        </div>
+        );
     }
 }
 
