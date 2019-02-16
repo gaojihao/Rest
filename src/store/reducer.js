7304842fa9f67f66cb0beb60eeb9c26c86b1de91
lizhi <lizhi1026@126.com>
2019-02-16 22:25:15 +0800

    add initialState

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,11 @@ import * as types from './actionTypes';
 
 // 给一个初始的 state
 const initialState = {
+    token:'',
+    userId:null,
+    method:'POST',
+    host:null,
+    url:null,
 }
 
 export default (state = initialState, action) => {
